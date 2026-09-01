d3b4d36bff8e5adc1d1bd15334916f5eb3dd77e2
Bristlefrost <pearsRdelicious@proton.me>
2026-08-31 21:35:29 -0500

    added my first JS file

---
+++ app.js
@@ -0,0 +1,8 @@
+const userName = "Bristlefrost-ka";
+console.log(`Hello, ${userName}! Welcome to your first Git project.`);
+
+function testGit() {
+    console.log("Git and VS Code skills: Active!");
+}
+
+testGit();
