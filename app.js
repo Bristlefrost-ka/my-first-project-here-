b97e230e6dd561fd603e183327dc48c53377fd37
Bristlefrost <pearsRdelicious@proton.me>
2026-09-01 09:26:40 -0500

    tryign things out

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const userName = "Bristlefrost-ka";
-console.log(`Hello, ${userName}! Welcome to your first Git project.`);
+var sketchProc = function(processingInstance) {
+    with (processingInstance) {
+        
+        size(400, 400); // Sets the canvas size
+        background(255, 255, 255); // White background
 
-function testGit() {
-    console.log("learning git + vscode! yaay");
-}
+        // Example Khan Academy drawing: A cute face
+        fill(255, 255, 0); // Yellow
+        ellipse(200, 200, 300, 300); // Face
 
-testGit();
+        fill(0, 0, 0); // Black eyes
+        ellipse(150, 150, 30, 30); 
+        ellipse(250, 150, 30, 30);
+
+    }
+};
+
+var canvas = document.getElementById("my-ka-art");
+var processingInstance = new Processing(canvas, sketchProc);
