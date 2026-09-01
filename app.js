7fe427d0842766a0080741c849ea70485e03f65b
Bristlefrost <pearsRdelicious@proton.me>
2026-09-01 09:27:51 -0500

    added art

--- app.js
+++ app.js
@@ -1,16 +1,199 @@
 var sketchProc = function(processingInstance) {
     with (processingInstance) {
         
-        size(400, 400); // Sets the canvas size
+        size(400, 500); // Sets the canvas size
         background(255, 255, 255); // White background
 
-        // Example Khan Academy drawing: A cute face
-        fill(255, 255, 0); // Yellow
-        ellipse(200, 200, 300, 300); // Face
+        // sophie's hand behind her
+        stroke(187, 125, 67);
+        fill(187, 125, 67);
+        beginShape();
+        vertex(124, 233);
+        bezierVertex(124, 233, 123, 235, 122, 233);
+        bezierVertex(119, 235, 107, 237, 111, 240);
+        bezierVertex(111, 240, 119, 242, 124, 233);
+        endShape();
 
-        fill(0, 0, 0); // Black eyes
-        ellipse(150, 150, 30, 30); 
-        ellipse(250, 150, 30, 30);
+        noStroke();
+        fill(107, 66, 40);
+        beginShape();
+        vertex(124, 234);
+        bezierVertex(124, 234, 118, 238, 111, 238);
+        bezierVertex(111, 238, 111, 238, 110, 238);
+        bezierVertex(110, 238, 108, 238, 109, 241);
+        bezierVertex(109, 241, 104, 246, 111, 249);
+        bezierVertex(111, 249, 111, 255, 115, 252);
+        bezierVertex(115, 252, 119, 248, 123, 248);
+        bezierVertex(123, 248, 124, 245, 126, 242);
+        bezierVertex(126, 242, 122, 238, 125, 234);
+        endShape();
+
+        fill(61, 48, 32);
+        beginShape();
+        vertex(111, 248);
+        bezierVertex(111, 248, 111, 247, 108, 245);
+        bezierVertex(108, 245, 112, 246, 112, 243);
+        bezierVertex(112, 243, 112, 246, 116, 245);
+        bezierVertex(116, 245, 113, 247, 111, 249);
+        endShape();
+
+        beginShape();
+        vertex(114, 252);
+        bezierVertex(114, 252, 117, 247, 122, 247);
+        bezierVertex(122, 247, 123, 244, 125, 242);
+        bezierVertex(125, 242, 127, 243, 127, 243);
+        bezierVertex(127, 243, 124, 251, 120, 250);
+        bezierVertex(120, 250, 117, 250, 112, 253);
+        endShape();
+
+        fill(231, 190, 136);
+        beginShape();
+        vertex(124, 233);
+        vertex(132, 233);
+        vertex(132, 237);
+        vertex(124, 238);
+        bezierVertex(124, 238, 123, 235, 124, 233);
+        endShape();
+
+        fill(170, 124, 89);
+        beginShape();
+        vertex(123, 237);
+        bezierVertex(123, 237, 124, 242, 126, 244);
+        bezierVertex(126, 244, 129, 244, 134, 241);
+        bezierVertex(134, 241, 133, 238, 130, 235);
+        bezierVertex(130, 235, 128, 236, 123, 237);
+        endShape();
+
+        // cape and face
+        fill(112, 65, 150);
+        beginShape();
+        vertex(156, 229);
+        bezierVertex(156, 229, 151, 227, 133, 231);
+        bezierVertex(131, 233, 132, 239, 135, 237);
+        bezierVertex(156, 230, 156, 230, 156, 230);
+        endShape();
+
+        fill(68, 39, 96);
+        beginShape();
+        vertex(154, 231);
+        bezierVertex(154, 231, 144, 231, 132, 234);
+        bezierVertex(132, 234, 132, 232, 134, 231);
+        bezierVertex(134, 231, 131, 232, 129, 239);
+        bezierVertex(129, 239, 131, 245, 136, 245);
+        bezierVertex(136, 245, 158, 240, 154, 230);
+        endShape();
+
+        fill(33, 34, 35);
+        beginShape();
+        vertex(160, 267);
+        bezierVertex(160, 267, 156, 270, 153, 271);
+        bezierVertex(153, 271, 161, 297, 170, 302);
+        bezierVertex(171, 308, 168, 312, 171, 314);
+        bezierVertex(171, 314, 183, 322, 209, 316);
+        bezierVertex(209, 316, 215, 315, 218, 314);
+        bezierVertex(218, 314, 219, 316, 224, 326);
+        bezierVertex(224, 326, 224, 328, 227, 328);
+        bezierVertex(227, 328, 229, 343, 222, 351);
+        bezierVertex(222, 351, 227, 354, 222, 356);
+        bezierVertex(222, 356, 214, 362, 188, 353);
+        bezierVertex(188, 353, 185, 353, 188, 357);
+        bezierVertex(188, 357, 206, 367, 220, 363);
+        bezierVertex(220, 363, 228, 361, 231, 359);
+        bezierVertex(231, 359, 244, 370, 257, 381);
+        bezierVertex(257, 381, 241, 354, 234, 354);
+        bezierVertex(234, 354, 234, 350, 232, 350);
+        bezierVertex(232, 350, 238, 344, 231, 341);
+        bezierVertex(231, 341, 234, 328, 235, 320);
+        bezierVertex(235, 320, 242, 325, 248, 328);
+        bezierVertex(248, 328, 252, 328, 259, 327);
+        bezierVertex(259, 327, 248, 324, 226, 310);
+        bezierVertex(226, 310, 234, 304, 244, 301);
+        bezierVertex(244, 301, 252, 300, 253, 302);
+        bezierVertex(253, 302, 260, 302, 271, 303);
+        bezierVertex(271, 303, 272, 297, 246, 292);
+        bezierVertex(246, 292, 253, 291, 238, 285);
+        bezierVertex(238, 285, 219, 280, 216, 277);
+        bezierVertex(216, 277, 219, 274, 219, 272);
+        bezierVertex(219, 272, 208, 265, 194, 260);
+        endShape();
+
+        fill(42, 44, 45);
+        beginShape();
+        vertex(165, 291);
+        vertex(160, 283);
+        bezierVertex(160, 283, 163, 295, 171, 301);
+        bezierVertex(171, 301, 174, 293, 179, 293);
+        bezierVertex(179, 293, 175, 287, 170, 286);
+        bezierVertex(170, 286, 167, 281, 165, 281);
+        bezierVertex(165, 281, 164, 291, 164, 290);
+        endShape();
+
+        beginShape();
+        vertex(194, 287);
+        vertex(202, 279);
+        bezierVertex(202, 279, 190, 287, 187, 291);
+        bezierVertex(187, 291, 181, 295, 175, 297);
+        bezierVertex(175, 297, 173, 302, 172, 304);
+        bezierVertex(172, 304, 177, 302, 177, 302);
+        bezierVertex(177, 302, 179, 306, 172, 311);
+        bezierVertex(172, 311, 189, 317, 213, 300);
+        bezierVertex(213, 300, 218, 298, 225, 293);
+        bezierVertex(225, 293, 194, 292, 178, 300);
+        bezierVertex(178, 300, 191, 290, 210, 283);
+        bezierVertex(210, 283, 202, 283, 195, 288);
+        endShape();
+
+        fill(135, 104, 57);
+        beginShape();
+        vertex(152, 280);
+        bezierVertex(152, 280, 150, 263, 154, 272);
+        bezierVertex(154, 272, 160, 288, 167, 299);
+        bezierVertex(167, 299, 171, 304, 164, 298);
+        bezierVertex(164, 298, 156, 289, 152, 277);
+        endShape();
+
+        fill(180, 135, 74);
+        beginShape();
+        vertex(171, 305);
+        bezierVertex(171, 305, 173, 295, 179, 293);
+        bezierVertex(179, 293, 185, 290, 185, 290);
+        bezierVertex(188, 283, 205, 273, 205, 273);
+        bezierVertex(202, 276, 191, 286, 191, 286);
+        bezierVertex(188, 289, 186, 292, 186, 292);
+        bezierVertex(181, 295, 176, 298, 176, 298);
+        bezierVertex(174, 300, 171, 305, 171, 305);
+        endShape();
+
+        fill(212, 166, 92);
+        beginShape();
+        vertex(171, 312);
+        bezierVertex(171, 312, 180, 318, 197, 310);
+        bezierVertex(197, 310, 229, 293, 235, 291);
+        bezierVertex(235, 291, 248, 291, 245, 294);
+        bezierVertex(245, 294, 217, 301, 201, 310);
+        bezierVertex(201, 310, 180, 318, 175, 315);
+        bezierVertex(175, 315, 173, 315, 170, 312);
+        endShape();
+
+        fill(194, 137, 79);
+        beginShape();
+        vertex(176, 284);
+        bezierVertex(176, 284, 179, 287, 179, 293);
+        bezierVertex(179, 293, 184, 290, 184, 286);
+        bezierVertex(184, 286, 181, 281, 177, 282);
+        endShape();
+
+        fill(114, 78, 46);
+        beginShape();
+        vertex(177, 282);
+        bezierVertex(177, 282, 184, 286, 181, 291);
+        bezierVertex(181, 291, 187, 286, 192, 282);
+        bezierVertex(192, 282, 197, 277, 199, 275);
+        bezierVertex(199, 275, 199, 266, 193, 264);
+        bezierVertex(193, 264, 182, 276, 179, 279);
+        endShape();
+
+
 
     }
 };
